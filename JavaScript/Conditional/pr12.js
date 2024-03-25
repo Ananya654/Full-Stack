290c46f284f0a463789f3a1df84cc9db1b9db66c
Ananya654 <ananyap654@gmail.com>
2024-03-25 12:22:51 +0530

    tst

---
+++ JavaScript/Conditional/pr12.js
@@ -0,0 +1,14 @@
+// JavaScript Program to find largest of Three Numbers?
+
+let a = 31;
+let b = 21;
+let c = 200;
+if(a>b && b>c){
+    console.log(" a is the largest of b and c.")
+}
+else if(b>a && a>c){
+    console.log(" b is the largest of a and c.")
+}
+else{
+    console.log(" c is the largest of a and b.")
+}
