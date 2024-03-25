ea29575aeccd55b0957f0f4aa5c0a1f445269555
Ananya654 <ananyap654@gmail.com>
2024-03-25 21:31:52 +0530

    tst

---
+++ JavaScript/Conditional/pr14.js
@@ -0,0 +1,9 @@
+// JS Program to check if a number is multiple of 3 or not using a ternary operator?
+
+let num = 171;
+ if(num%3 == 0){
+    console.log(num +" is multiple of 3. ")
+ }
+ else{
+    console.log(num +" is not a multiple of 3. ")
+ }
