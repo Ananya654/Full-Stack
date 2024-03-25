9d82643b21e52f733ed33968202d7084997ae349
Ananya654 <ananyap654@gmail.com>
2024-03-25 22:02:51 +0530

    tst

---
+++ JavaScript/Conditional/pr16.js
@@ -0,0 +1,6 @@
+//JS Program to check if a number is 3-digit numbers using a ternary operator?
+
+let n = 9991;
+let num = (n>=100 && n<=999) ? n+ " the number is three digit number." : n +" the number is not three digit number."
+
+console.log(num);
---
+++ JavaScript/Conditional/pr17.js
@@ -0,0 +1,7 @@
+//Write a program to print even or odd for a given number using a ternary operator?
+
+
+let n = 2212;
+let num = (n%2 == 0) ? n+ " the number is  even number." : n +" the number is odd number."
+
+console.log(num);
