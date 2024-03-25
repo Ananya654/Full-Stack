21b147373da5c269b07d5b96c3b54287d3a5418f
Ananya654 <ananyap654@gmail.com>
2024-03-25 21:40:56 +0530

    tst

---
+++ JavaScript/Conditional/pr15.js
@@ -0,0 +1,12 @@
+//Write a program to check if a number is positive or not using a ternary operator?
+
+let n = -10;
+if(n>0){
+    console.log(n +" is a positive number.")
+}
+else if(n==0){
+    console.log(n +" is a neither positive nor negative number.")
+}
+else{
+    console.log(n +" is a negative number.")
+}
