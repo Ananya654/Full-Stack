511587af8929a04b0619b7ce0b98e680336c9b1b
Ananya654 <ananyap654@gmail.com>
2024-03-25 12:33:08 +0530

    tst

---
+++ JavaScript/Conditional/pr13.js
@@ -0,0 +1,9 @@
+//JS Program check if a number is divisible by 7 or not using a ternary operator
+
+let num = 21;
+ if(num%7 == 0){
+    console.log(num +" is divisible by 7. ")
+ }
+ else{
+    console.log(num +" is not divisible by 7. ")
+ }
