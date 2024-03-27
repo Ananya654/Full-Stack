69a221c8b5309751c51f4845a8459cdb1332cfff
Ananya654 <ananyap654@gmail.com>
2024-03-27 13:15:41 +0530

    tst

--- JavaScript/Iterative/pr38.js
+++ JavaScript/Iterative/pr38.js
@@ -1,10 +1,8 @@
 // Write a program to print 1 to 10 even numbers using a for loop
 
-for(let num = 0 ; num <= 10 ; num++){
+for(let num = 1 ; num <= 10 ; num++){
     if(num%2 == 0){
         console.log(num + " is a even number.")
     }
-    else{
-        console.log(num + " is not a even number.")
-    }
+    
 }
---
+++ JavaScript/Iterative/pr39.js
@@ -0,0 +1,8 @@
+//Write a program to print 1 to 10 odd numbers using For Loop
+
+for(let num = 1 ; num <= 10 ; num++){
+    if(num%2 != 0){
+        console.log(num + " is a odd number.")
+    }
+    
+}
